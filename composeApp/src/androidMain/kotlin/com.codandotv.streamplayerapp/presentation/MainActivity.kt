ff5dfbbf14faf63fe4e296b61ac28b13c447c4e9
Pedro Alvarez <14023675+pnalvarez@users.noreply.github.com>
2025-05-15 22:41:27 -0300

    added crashlytics

--- composeApp/src/androidMain/kotlin/com.codandotv.streamplayerapp/presentation/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com.codandotv.streamplayerapp/presentation/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.codandotv.streamplayerapp.StreamPlayerApp
 import com.google.firebase.Firebase
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.initialize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Firebase.initialize(this)
+        FirebaseCrashlytics.getInstance().log("Logging crash message:")
+        throw RuntimeException("Custom error")
         setContent {
             StreamPlayerApp()
         }
