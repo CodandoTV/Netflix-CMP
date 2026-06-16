9470bafd4350d235d3685abc86c663319ebf4026
gabrielmoro <gabrielbronzattimoro.es@gmail.com>
2026-06-16 20:39:23 -0300

    fix: add missing newlines at the end of several files

--- core-shared-ui/src/commonMain/kotlin/com/codandotv/streamplayerapp/core/shared/ui/widget/StreamPlayerTopBar.kt
+++ core-shared-ui/src/commonMain/kotlin/com/codandotv/streamplayerapp/core/shared/ui/widget/StreamPlayerTopBar.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,19 +21,15 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
 import com.codandotv.streamplayerapp.core.shared.ui.resources.Colors
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import streamplayerapp_kmp.core_shared_ui.generated.resources.Res
 import streamplayerapp_kmp.core_shared_ui.generated.resources.ic_netflix
 import streamplayerapp_kmp.core_shared_ui.generated.resources.icon_netflix
-import streamplayerapp_kmp.core_shared_ui.generated.resources.icon_profile
 import streamplayerapp_kmp.core_shared_ui.generated.resources.icon_search
-import streamplayerapp_kmp.core_shared_ui.generated.resources.perfil_fake
 import streamplayerapp_kmp.core_shared_ui.generated.resources.topbar_categories
 import streamplayerapp_kmp.core_shared_ui.generated.resources.topbar_movies
 import streamplayerapp_kmp.core_shared_ui.generated.resources.topbar_shows
